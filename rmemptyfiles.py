8601bfb4db4f3ebc8e42fafbb5301ed49278565a
kwikadi <kwikadi@gmail.com>
2015-01-04 19:20:18 +0530

    Multiple argument support included

--- rmemptyfiles.py
+++ rmemptyfiles.py
@@ -1,10 +1,11 @@
 import os, sys
 
-source = str(sys.argv[1])
+sources = str(sys.argv)
 
-for root,dirs,files in os.walk(source):
-	for name in files:
-		paths = os.path.join(root,name)
-		if os.stat(paths).st_size == 0:
-			print paths
-			os.remove(paths)
+for source in sources:
+	for root,dirs,files in os.walk(source):
+		for name in files:
+			paths = os.path.join(root,name)
+			if os.stat(paths).st_size == 0:
+				print paths
+				os.remove(paths)
